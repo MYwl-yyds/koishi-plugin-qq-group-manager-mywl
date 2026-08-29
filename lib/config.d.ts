10d5cca22a1a7a304a5884a53d79f264529da9ae
末影MYwl <120080994+MYwl-yyds@users.noreply.github.com>
2026-08-29 11:52:02 +0800

    Delete lib directory

--- lib/config.d.ts
+++
@@ -1,8 +0,0 @@
-import { Schema } from 'koishi';
-export declare const name = "quanmian-qq-guanqun";
-export interface PluginConfig {
-    enableGroupManagement: boolean;
-    superUsers: string[];
-    onebotFramework: 'auto' | 'napcat' | 'llbot';
-}
-export declare const Config: Schema<PluginConfig>;
